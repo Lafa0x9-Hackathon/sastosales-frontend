5908e865135114134f12fbc4b63d7d62a6663aef
Sushma Acharya <sushmaacharya345@gmail.com>
2024-04-12 12:13:55 +0545

    review section added

---
+++ src/components/review/review.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const review = () => {
+  return (
+    <div>
+        <div className='review-container'>
+            <div className='popup'>
+                <h1>Write a review</h1>
+            </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default review

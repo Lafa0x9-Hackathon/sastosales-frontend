5c2d80465b5ed1ad6edabeeeb1c6e3281066e5cf
rajesh pandey <rajeshpandry24@gmail.com>
2024-04-14 21:39:06 +0545

    more product details cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
+import React from "react";
+import BasicProductDetails from "./productAdded/ProductDetailCart";
 
-function App() {
+const App = () => {
   return (
-    <div>
-        sasto sales
-    </div>
+    <>
+      <BasicProductDetails />
+    </>
   );
+};
 
-}
-
-export default App
+export default App;
---
+++ src/productAdded/compontnts/pegination.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "../css/pegination.css";
+
+const pegination = () => {
+  return (
+    <>
+      <div className="buttons">
+        <button className="btn "> Previous</button>
+        <button className="btn ">Next 1/3 </button>
+      </div>
+    </>
+  );
+};
+
+export default pegination;

aee3969b59e358284085dfa89df1c1f396813804
Git-Hub452 <paudelsabina345@gmail.com>
2024-04-14 16:12:08 +0545

    shopping cart/ by sabina

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-
+import ProductDetails from "./Component/ProductDetails";
+import { ShoppingCard } from "./Component/ShoppingCard";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from 'react';
 function App() {
   return (
     <div>
-        sasto sales
+       <BrowserRouter>
+  <Routes>
+  <Route path="/" element={<ShoppingCard />} />
+  <Route path="/product-details" element={<ProductDetails />} />
+  </Routes>
+  </BrowserRouter>
     </div>
+   
   );
 
 }
+
+
 
 export default App

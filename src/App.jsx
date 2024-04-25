f7b6936a48f4fba15b1a4a8350ccf3e7d4048e15
Sushma Acharya <sushmaacharya345@gmail.com>
2024-04-25 11:02:14 +0545

    Delete aap.jsx some data

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,4 @@
-// import ProductDetails from "./Component/ProductDetails";
-// import { ShoppingCard } from "./Component/ShoppingCard";
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import React from 'react';
-// function App() {
-//   return (
-//     <div>
-//        <BrowserRouter>
-//   <Routes>
-//   <Route path="/" element={<ShoppingCard />} />
-//   <Route path="/product-details" element={<ProductDetails />} />
-//   </Routes>
-//   </BrowserRouter>
-//     </div>
-   
-//   );
-// }
+
 
 
 
